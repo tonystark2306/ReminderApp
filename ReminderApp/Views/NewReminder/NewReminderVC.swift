fdd8e01e511a1ceee4a1f2510a94ea7c4db63099
Tuấn Đạt <doctorstrangek37d@gmail.com>
2025-08-25 18:26:01 +0700

    add text field for add reminder vc

--- ReminderApp/Views/NewReminder/NewReminderVC.swift
+++ ReminderApp/Views/NewReminder/NewReminderVC.swift
@@ -9,21 +9,58 @@ import UIKit
 
 class NewReminderVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private var titleText: String = ""
+    private var descriptionText: String = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupTableView() {
+        tableView.register(UINib(nibName: "TextFieldCell", bundle: nil),
+                           forCellReuseIdentifier: "TextFieldCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 52
+        tableView.keyboardDismissMode = .onDrag
+        tableView.layer.cornerRadius = 16
     }
-    */
 
+}
+
+extension NewReminderVC: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 2 }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "TextFieldCell",
+            for: indexPath
+        ) as! TextFieldCell
+
+        if indexPath.row == 0 {
+            // Cell Title
+            cell.textField.placeholder = "Title"
+            cell.textField.font = .systemFont(ofSize: 17, weight: .regular)
+            cell.textField.textColor = .neutral3
+            cell.textField.returnKeyType = .next
+            cell.onTextChanged = { [weak self] text in
+                self?.titleText = text
+            }
+        } else {
+            // Cell Description
+            cell.textField.placeholder = "Description"
+            cell.textField.font = .systemFont(ofSize: 17, weight: .regular)
+            cell.textField.textColor = .neutral3
+            cell.textField.returnKeyType = .done
+            cell.onTextChanged = { [weak self] text in
+                self?.descriptionText = text
+            }
+        }
+
+        return cell
+    }
 }
